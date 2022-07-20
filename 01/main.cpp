aefeb629d4771d15d93ea72a719483e32d152d6b
Jacky <fansai320@gmail.com>
2022-07-20 13:54:26 +0800

    cmake加库引用 01注释

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,4 +1,4 @@
-#include "Triangle.hpp"
+﻿#include "Triangle.hpp"
 #include "rasterizer.hpp"
 #include <Eigen/Eigen>
 #include <iostream>
@@ -34,9 +34,32 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
              0,0,1,0,
              0,0,0,1;
 
+    //model << //绕x轴
+   //    1, 0, 0, 0,
+   //    0, std::cos(angle), -std::sin(angle), 0,
+   //    0, std::sin(angle), std::cos(angle), 0,
+   //    0, 0, 0, 1;
+   //model << //绕y轴
+   //    std::cos(angle), 0, std::sin(angle), 0,
+   //    0, 1, 0, 0,
+   //    -std::sin(angle), 0, std::cos(angle), 0,
+   //    0, 0, 0, 1;
+
     return model;
 }
 
+//投影可以看这个
+//https://zhuanlan.zhihu.com/p/45757899
+//https://zhuanlan.zhihu.com/p/142943803
+//https://zhuanlan.zhihu.com/p/448904350
+// https://zhuanlan.zhihu.com/p/361156478
+//eyefov表示视野角度，aspect_ratio表示xy的比例
+//前文提到，透视变换即就是先透视投影变换，再做正交投影变换。所以可以先写出透视投影矩阵（其中参数只有n, f），然后通过计算出b, t, l, r的值，写出正交投影的矩阵，二者相乘可得到结果
+//透视投影，根据名字判断，4个参数分别是：上下可视角度，长宽比width / height，z近距离，z远距离（分别为视频5,5,4,4提到的内容）
+//https://zhuanlan.zhihu.com/p/386137659
+//截面与远截面之间构成的四棱台称为棱台观察体，而透视投影矩阵的任务就是把位于观察体内的物体的顶点 x, y, z x, y, zx, y, z 坐标映射到[− 1, 1][-1, 1][−1, 1] 范围。
+//这相当于把这个四棱台扭曲变形成一个立方体。这个立方体叫做规范化观察体（Normalized View Volume)
+//https://zhuanlan.zhihu.com/p/463027517
 Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
                                       float zNear, float zFar)
 {
@@ -48,13 +71,39 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     // Create the projection matrix for the given parameters.
     // Then return it.
 
-    float top = -tan(DEG2RAD(eye_fov/2.0f) * abs(zNear));
+    /*float top = -tan(DEG2RAD(eye_fov/2.0f) * abs(zNear));
     float right = top * aspect_ratio;
 
     projection << zNear/right,0,0,0,
                   0,zNear/top,0,0,
                   0,0,(zNear+zFar)/(zNear-zFar),(2*zNear*zFar)/(zFar-zNear),
-                  0,0,1,0;
+                  0,0,1,0;*/
+    Eigen::Matrix4f m;
+    m << zNear, 0, 0, 0,
+        0, zNear, 0, 0,
+        0, 0, zNear + zFar, -zNear * zFar,
+        0, 0, 1, 0;////透视投影矩阵
+
+    float halve = eye_fov / 2 * MY_PI / 180;
+    float top = tan(halve) * zNear;
+    float bottom = -top;
+    float right = top * aspect_ratio;
+    float left = -right;
+
+    Eigen::Matrix4f n, p;
+    //这个是将立方体进行规范化（-1，1）
+    n << 2 / (right - left), 0, 0, 0,
+        0, 2 / (top - bottom), 0, 0,
+        0, 0, 2 / (zNear - zFar), 0,
+        0, 0, 0, 1;
+
+    //这里是将三角形位移到原点
+    p << 1, 0, 0, -(right + left) / 2,
+        0, 1, 0, -(top + bottom) / 2,
+        0, 0, 1, -(zFar + zNear) / 2,
+        0, 0, 0, 1;
+
+    projection = n * p * m;//这里是左乘所以是先进行透视转正交，然后位移，然后规范化
 
     return projection;
 }
@@ -77,7 +126,7 @@ int main(int argc, const char** argv)
 
     rst::rasterizer r(700, 700);
 
-    Eigen::Vector3f eye_pos = {0, 0, 5};
+    Eigen::Vector3f eye_pos = {0, 0, 5}; //眼睛所在位置
 
     std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
 
@@ -105,11 +154,11 @@ int main(int argc, const char** argv)
         return 0;
     }
 
-    while (key != 27) {
+    while (key != 27) { //esc
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
         r.set_model(get_model_matrix(angle));
-        r.set_view(get_view_matrix(eye_pos));
+        r.set_view(get_view_matrix(eye_pos)); //[0][1][2] 0,0,5
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
         r.draw(pos_id, ind_id, rst::Primitive::Triangle);
--- 01/rasterizer.cpp
+++ 01/rasterizer.cpp
@@ -11,6 +11,10 @@
 
 rst::pos_buf_id rst::rasterizer::load_positions(const std::vector<Eigen::Vector3f> &positions)
 {
+    /*map 容器的成员函数 emplace() 可以在适当的位置直接构造新元素，从而避免复制和移动操作。它的参数通常是构造元素，也就是 pair<const K, T> 对象所需要的。
+    只有当容器中现有元素的键与这个元素的键不同时，才会构造这个元素。下面是一个示例：
+        std::map<Name, size_t> people;
+    auto pr = people.emplace(Name{ "Dan","Druff" }, 77);*/
     auto id = get_next_id();
     pos_buf.emplace(id, positions);
 
@@ -132,6 +136,7 @@ auto to_vec4(const Eigen::Vector3f& v3, float w = 1.0f)
     return Vector4f(v3.x(), v3.y(), v3.z(), w);
 }
 
+//pos_buffer和ind_buffer都是模仿opengl,从buffer中通过index取出元素
 void rst::rasterizer::draw(rst::pos_buf_id pos_buffer, rst::ind_buf_id ind_buffer, rst::Primitive type)
 {
     if (type != rst::Primitive::Triangle)
@@ -141,14 +146,22 @@ void rst::rasterizer::draw(rst::pos_buf_id pos_buffer, rst::ind_buf_id ind_buffe
     auto& buf = pos_buf[pos_buffer.pos_id];
     auto& ind = ind_buf[ind_buffer.ind_id];
 
+  /*  首先buf, ind两个量分别将之前加载到光栅化器中的三角形坐标和索引取出。f1和f2两个量是为了之后拉伸z轴从（ - 1, 1）的标准立方体到（0, 100）
+        （虽然我也不知道为啥这里要拉伸z轴，可能是opencv绘图的标准接口？）总之将mvp变换矩阵分别乘以三角形三个顶点的坐标，最终将三角形转化到[-1, 1] ^ 3的标准立方体中。
+        再通过视口变换，将x方向从[-1, 1]变为[0, width], y方向从[-1, 1]变到[0, height]，z方向由[-1, 1]变到[0, 100]左右。
+        （做完视口变换，这时三角形的三个点坐标就和最终要显示在屏幕上的像素对应起来了）。*/
+
     float f1 = (100 - 0.1) / 2.0;
     float f2 = (100 + 0.1) / 2.0;
+    std::cout << " pos_id:" << pos_buffer.pos_id << " ind_id:" << ind_buffer.ind_id << std::endl;
 
+    //将三维空间中的三角形转为2d空间中的
     Eigen::Matrix4f mvp = projection * view * model;
     for (auto& i : ind)
     {
         Triangle t;
-
+        std::cout << "ind i:" << std::endl;
+        //转为2d空间
         Eigen::Vector4f v[] = {
                 mvp * to_vec4(buf[i[0]], 1.0f),
                 mvp * to_vec4(buf[i[1]], 1.0f),
@@ -156,18 +169,19 @@ void rst::rasterizer::draw(rst::pos_buf_id pos_buffer, rst::ind_buf_id ind_buffe
         };
 
         for (auto& vec : v) {
-            vec /= vec.w();
+            vec /= vec.w();//归一化，之后最后一个元素w为1
         }
 
         for (auto & vert : v)
         {
-            vert.x() = 0.5*width*(vert.x()+1.0);
+            vert.x() = 0.5*width*(vert.x()+1.0);//width是屏幕宽度,height是屏幕高度
             vert.y() = 0.5*height*(vert.y()+1.0);
             vert.z() = vert.z() * f1 + f2;
         }
 
         for (int i = 0; i < 3; ++i)
         {
+            //head<n>()函数：对变量Eigen::Vector4f x进行x.head<n>()操作表示提取前n个元素
             t.setVertex(i, v[i].head<3>());
             t.setVertex(i, v[i].head<3>());
             t.setVertex(i, v[i].head<3>());
@@ -203,6 +217,9 @@ void rst::rasterizer::set_projection(const Eigen::Matrix4f& p)
     projection = p;
 }
 
+//可以看到方法是将framebuf（即屏幕的像素数组，本项目中定义为700x700），用vector3f{ 0,0,0 }来填充，该RGB三通道信息对应为纯黑色。
+//depth_buf（深度数组，代表每个点的深度信息，将在光栅化和渲染部分用到，本次实验可忽略）用infinity()（无穷大）来填充，这个函数即将全屏幕所有像素点都变为黑色，
+//可以视作屏幕清空或者初始化。
 void rst::rasterizer::clear(rst::Buffers buff)
 {
     if ((buff & rst::Buffers::Color) == rst::Buffers::Color)
